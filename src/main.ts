caa59f761890f5502456feb4cad635afa104f79f
mosesmmoisebidth <mosesmanek7@gmail.com>
2025-01-21 21:05:57 +0200

    allow all cookies

--- src/main.ts
+++ src/main.ts
@@ -24,8 +24,10 @@ async function bootstrap() {
     app.use(compression());
     console.log("enabling cors")
     app.enableCors({
-      origin: ['http://localhost:3000', 'https://jivah.vercel.app'],
-      credentials: true,
+      origin: ['http://localhost:3000', 'https://jivah.vercel.app'], // Allow specific origins
+      credentials: true, // Allow cookies to be sent with requests
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Explicitly allow HTTP methods
+      allowedHeaders: 'Content-Type,Authorization', // Explicitly allow headers
     });
     app.use(cookieParser());
     app.enableVersioning();
