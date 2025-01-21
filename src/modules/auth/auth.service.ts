bb841ed6be4f327f4393d92fb572db175c21e613
mosesmmoisebidth <mosesmanek7@gmail.com>
2025-01-21 17:50:22 +0200

    changes the process

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -86,13 +86,13 @@ import { TimeoutError } from 'rxjs';
       const tokens = await this.tokenService.generateTokens(user);
       res.cookie('accessToken', tokens.accessToken), {
         httpOnly: true,
-        secure: process.env.IS_DEV ? false : false,
+        secure: process.env.NODE_ENV === 'production',
         sameSite: 'lax',
         maxAge: 1000 * 60 * 20
       }
       res.cookie('refreshToken', tokens.refreshToken, {
         httpOnly: true,
-        secure: process.env.IS_DEV ? false: false,
+        secure: process.env.NODE_ENV === 'production',
         sameSite: 'lax',
         maxAge: 1000 * 60 * 60 * 24 * 30, // 7 days for refreshToken
       });
